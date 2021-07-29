cca67978e81d8a0472fb358878e39703013ef5e9
radittya <radittyaajiwibowo@gmail.com>
2021-07-29 20:45:02 +0700

    ngumpulin tugas latihan 3

--- Sprint 2/Latihan 3/Soal 1/object.js
+++ Sprint 2/Latihan 3/Soal 1/object.js
@@ -1,18 +1,27 @@
-//object1
-function makanan (namaMakanan,harga) {
-    return{
-        "namaMakanan" : namaMakanan,
-        "harga" : harga
-    }
-}
+var RadittyaAjiWibowo ={
+//Object1
+"kipas angin" :{
+    "merek":'sonic',
+    "berat":5,
+    "warna":'biru dongker',
+    on:function(menghidupkan) {
+        return 'menghidupkan kipas angin'
 
-console.log(makanan("nasi padang",12000))
+
+    },
+    off:function(mati) {
+        return 'mematikan kipas angin'
+      
+    }
+},
+
+
 
 //object2
-var laptop = {
-    merek:'lenovo',
-    berat:1.5,
-    warna:'biru dongker',
+ "laptop" :{
+    "merek":'lenovo',
+    "berat":1.5,
+    "warna":'biru dongker',
     on:function(menghidupkan) {
         return 'laptop'
 
@@ -22,18 +31,129 @@ var laptop = {
         return 'mematikan laptop'
       
     }
-}
-console.log(laptop)
+},
+
 
 //object3
+" mesin cuci ": {
+    "merek":'lg',
+    "berat":10,
+    "warna":'pink',
+    on:function(menghidupkan) {
+        return 'menjalankan mesin cuci'
 
-function minuman(namaMinuman,harga) {
-    return{
-        "nama minuman" : namaMinuman,
-        "harga" : harga
+
+    },
+    off:function(mematikan) {
+        return 'menghentiakn mesin cuci'
+      
     }
+},
+
+
+//object4
+" motor ": {
+    "merek":'custom cooper mini',
+    "berat":150,
+    "warna":'item dop',
+    gas:function(menghidupkan) {
+        return 'menjalankan motor'
+
+
+    },
+    rem:function(mematikan) {
+        return 'menghentiakn motor'
+      
+    }
+},
+
+
+//object5
+ 'handphone' : {
+    "merek":'oddo',
+    "berat":500,
+    "warna":'item ',
+    on:function(menghidupkan) {
+        return 'menghidupkan handphone'
+
+
+    },
+    off:function(mematikan) {
+        return 'mematikan handphone'
+      
+    }
+},
+
+
+//object6
+ "mobil" : {
+    "merek":'honda brio',
+    "berat":600,
+    "warna":'item',
+    gas:function(menghidupkan) {
+        return 'menjalankan mobil'
+
+
+    },
+    rem:function(mematikan) {
+        return 'menghentiakn mobil'
+      
+    }
+},
+
+
+//object7
+
+ "earhone" : {
+    "merek":'jbl',
+    "berat":300,
+    "warna":'item',
+    dimasukan:function(menghidupkan) {
+        return 'menjalankan eaerphone'
+
+
+    },
+    dilepas:function(mematikan) {
+        return 'menghentiakn earphone'
+      
+    }
+},
+//object8
+
+"buku" : {
+    "merek":'sidu',
+    "berat":1.,
+    "warna":'item',
+    dibaca:function() {
+        return 'ayo membaca buku'
+      
+    }
+},
+//object9
+"miinyak rambut" : {
+    "merek":'mustika ratu',
+    "berat":0.5,
+    "warna":'ijo',
+    digunakan:function() {
+        return 'untuk merawat rambut'
+
+
+  
+      
+    }
+},
+//Object
+"mendoan" : {
+    "merek":'khas cilacap',
+    "jumlah":'10',
+    "warna":'kuning',
+    dimakan:function() {
+        return 'mendoan sangat enak'
+
+
+   
+      
+    }
+},
 }
-console.log(minuman("ice chocolatos",10000))
-
-
-
+console.log (RadittyaAjiWibowo)
---
+++ Sprint 2/Latihan 3/Soal 3/object2.js
@@ -0,0 +1,40 @@
+function arrayto0bject(arr){
+    var date = new Date ()
+    var data1 = date.getFullYear() - arr["1. Abduh Muhamad"][3]
+    var data2 = date.getFullYear() - arr["2. Ahmad Taufik"][3]
+
+    var people1 = {
+        firstName : arr ["1. Abduh Muhamad"][0],
+        lastName  : arr ["1. Abduh Muhamad"][1],
+        gander    : arr ["1. Abduh Muhamad"][2],
+        age       : data1
+    }
+
+    var people2 = {
+        firstName : arr ["2. Ahmad Taufik"][0],
+        lastName  : arr ["2. Ahmad Taufik"][1],
+        gander    : arr ["2. Ahmad Taufik"][2],
+        age       : data2
+    }
+
+    console.log('1. Abduh Muhamad : ',people1);
+    console.log('\n');
+    console.log('2. Ahmad Taufik  :',people2);
+}
+
+var people = {
+    "1. Abduh Muhamad" : [
+        "Abduh",
+        "Muhammad",
+        "male",
+        1992,
+    ],
+    "2. Ahmad Taufik" : [
+        "Ahmad",
+        "Taufik",
+        "male",
+        1985
+    ]
+}
+
+arrayToObject(people) 
