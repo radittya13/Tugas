ee22e6a724a1cb3b8d81a2ea5a84bd52ff224c95
radittya <radittyaajiwibowo@gmail.com>
2021-07-31 07:00:54 +0700

    kumpul tugas

---
+++ Sprint 2/latihan 5/soalno1.js
@@ -0,0 +1,10 @@
+for( angka = 1; angka <= 20; angka++){
+    if( angka % 2 != 0 && angka % 3 == 0){
+     console.log(angka + '-' + 'I Love Coding');
+    }else if(angka % 2 != 0){
+        console.log(angka  + '-' + 'Santai');
+    }else if(angka % 2 == 0){
+        console.log(angka  + '-' + 'Berkualitas' )
+        
+    }
+ }
---
+++ Sprint 2/latihan 5/soalno2.js
@@ -0,0 +1,7 @@
+function kalikan(num1,num2){
+    return '48'
+}
+var num1=12;
+var num2=4;
+var hasilKali=kalikan(num1,num2)
+console.log( hasilKali)
